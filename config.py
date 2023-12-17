0abf7bf544be21518ef0736796e19c7f918eb996
oguz <oguzhankir17@gmail.com>
2023-12-17 15:54:33 +0300

    fix: config

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ class Config:
     whisper_model_type = "large"
     embedding_model = "speechbrain/spkrec-ecapa-voxceleb"
     openai_api_key = "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"
-    user_history_path = "/home/oguz/Desktop/personal/speech-graph-generator/data/call_data/records/user1"
+    user_history_path = "/home/oguz/Desktop/personal/AutoConsult/data/call_data/records/user1"
     sales_asistant_dialog_path = os.path.join(user_history_path,
                                               sorted([pd.to_datetime(f) for f in os.listdir(user_history_path)])[
                                                   -1].strftime(
                                                   format="%d%m%y-%H%M%S"))
-    graph_data_path = "/home/oguz/Desktop/personal/speech-graph-generator/data/graph_data"
+    graph_data_path = "/home/oguz/Desktop/personal/AutoConsult/data/graph_data"
     NEO4J_URI = "neo4j+s://725f44d4.databases.neo4j.io"
     NEO4J_USERNAME = "neo4j"
     NEO4J_PASSWORD = "NtCli2aXlH3YgnTERiEPG92baITF9ZbArkki1o1e0XM"
