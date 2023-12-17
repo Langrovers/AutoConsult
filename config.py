82940e542f745e128561a41c597cfc546c53e466
oguz <oguzhankir17@gmail.com>
2023-12-17 14:06:16 +0300

    fix: config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
+import pandas as pd
 import torch
 
 
@@ -7,14 +10,15 @@ class Config:
     embedding_model = "speechbrain/spkrec-ecapa-voxceleb"
     openai_api_key = "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"
     user_history_path = "/home/oguz/Desktop/personal/speech-graph-generator/data/call_data/records/user1"
-    sales_asistant_dialog_path = "/home/oguz/Desktop/personal/speech-graph-generator/data/call_data/records/user1/131223-202906"
+    sales_asistant_dialog_path = os.path.join(user_history_path,
+                                              sorted([pd.to_datetime(f) for f in os.listdir(user_history_path)])[
+                                                  -1].strftime(
+                                                  format="%d%m%y-%H%M%S"))
     graph_data_path = "/home/oguz/Desktop/personal/speech-graph-generator/data/graph_data"
-    NEO4J_URI="neo4j+s://725f44d4.databases.neo4j.io"
+    NEO4J_URI = "neo4j+s://725f44d4.databases.neo4j.io"
     NEO4J_USERNAME = "neo4j"
-    NEO4J_PASSWORD="NtCli2aXlH3YgnTERiEPG92baITF9ZbArkki1o1e0XM"
-    AURA_INSTANCEID="725f44d4"
-    AURA_INSTANCENAME="Instance01"
+    NEO4J_PASSWORD = "NtCli2aXlH3YgnTERiEPG92baITF9ZbArkki1o1e0XM"
+    AURA_INSTANCEID = "725f44d4"
+    AURA_INSTANCENAME = "Instance01"
     GENERATE_CYPER = False
     INGEST_NEO4J = False
-
-
