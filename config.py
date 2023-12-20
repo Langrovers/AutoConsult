ab2dae53bb436e51066cc132744d79ae1d0f8877
oguz <oguzhankir17@gmail.com>
2023-12-20 17:41:26 +0300

    update config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import torch
 class Config:
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     whisper_model_type = "large"
-    embedding_model = "speechbrain/spkrec-ecapa-voxceleb"
+    embedding_model = ""
     openai_api_key = "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"
     user_history_path = "/home/oguz/Desktop/personal/AutoConsult/data/call_data/records/user1"
     sales_asistant_dialog_path = os.path.join(user_history_path,
@@ -15,10 +15,10 @@ class Config:
                                                   -1].strftime(
                                                   format="%d%m%y-%H%M%S"))
     graph_data_path = "/home/oguz/Desktop/personal/AutoConsult/data/graph_data"
-    NEO4J_URI = "neo4j+s://725f44d4.databases.neo4j.io"
-    NEO4J_USERNAME = "neo4j"
-    NEO4J_PASSWORD = "NtCli2aXlH3YgnTERiEPG92baITF9ZbArkki1o1e0XM"
-    AURA_INSTANCEID = "725f44d4"
-    AURA_INSTANCENAME = "Instance01"
+    NEO4J_URI = ""
+    NEO4J_USERNAME = ""
+    NEO4J_PASSWORD = ""
+    AURA_INSTANCEID = ""
+    AURA_INSTANCENAME = ""
     GENERATE_CYPER = False
     INGEST_NEO4J = False
